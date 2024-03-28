9dc5cc150d5deff5a79f5d6c6af09277db1842c9
el3v3n0110 <arorapavitra34@gmail.com>
2024-03-28 15:37:16 +0530

    commit to make it live

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,10 +10,10 @@ function Navbar() {
         <div className="p-2 flex ">
           <img src={logo} alt="logo" className=" h-14" />
           <h3
-            className=" font-bold text-bjp-orange my-auto
+            className=" font-bold text-3xl text-bjp-orange my-auto
              "
           >
-            BHARTIYA JANTA PARTY
+            Hindustani Awam Morcha
           </h3>
         </div>
         <div className="left flex ">
@@ -21,13 +21,13 @@ function Navbar() {
           <div className="px-2 hover:text-bjp-orange">UPCOMING EVENTS</div>
           <div className="px-2 hover:text-bjp-orange">MEDIA RESOURCES</div>
           <div className="px-2 hover:text-bjp-orange">KAMAL SANDESH</div>
-          <div className="px-2 hover:text-bjp-orange"> BJP LIVE</div>
+          <div className="px-2 hover:text-bjp-orange"> HAM LIVE</div>
           <div className=""></div>
            
         </div>
         <div className="flex px-2 ">
 
-          <button className=" bg-bjp-orange m-5 px-10 rounded  "> JOIN BJP</button>
+          <button className=" bg-bjp-orange m-5 px-10 rounded  "> JOIN HAM</button>
           <button className=" bg-bjp-saffron my-5 px-2 rounded "> MAKE A DONATION</button>
           <Icon className="my-auto " path={mdiNavigation} size={1} />
         </div>
